c25fc56c21a26b923cbfeab613985a7771407ac6
rolandj <36603341+jroland1994@users.noreply.github.com>
2018-02-18 17:52:01 +0100

    Update Player.java
    Removed String armor and weapon, added armor and damage to int above

--- Player.java
+++ Player.java
@@ -1,10 +1,10 @@
 public class Player
     {
         //Variables for a character, effectivley, the character sheet:
-        int strength=1, dexterity=1, intelligence=1, speed=dexterity*4, skillcount=intelligence+1;
+        int strength=1, dexterity=1, intelligence=1, armor, damage, speed=dexterity*4, skillcount=intelligence+1;
         static int money, health, mana;
         static boolean living, bleeding, stealth;
-        String name, armor, weapon; 
+        String name;
         String[] skills = new String[skillcount], inventory = new String[6];
         //Initialization class. Initialises beginer variable values for HP, MP, and livign status. Call after cahr creating when staring game:
         public void init()
